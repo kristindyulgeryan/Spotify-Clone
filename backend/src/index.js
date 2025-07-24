7c48f40eecc97dfd1c7f49e73fbea02b231f4158
Kristin <kristindyulgeryan@abv.bg>
2025-07-24 16:53:53 +0300

    Add MongoDB connection utility using mongoose

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,6 +6,7 @@ import authRoutes from "./routes/auth.route.js";
 import songRoutes from "./routes/song.route.js";
 import albumRoutes from "./routes/album.route.js";
 import statRoutes from "./routes/stat.route.js";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
@@ -21,4 +22,5 @@ app.use("/api/stats", statRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
+  connectDB();
 });
