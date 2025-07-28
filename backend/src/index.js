0e426f68757d0e0c70734bbd316b3dc3a8d2fb96
Kristin <kristindyulgeryan@abv.bg>
2025-07-28 15:29:59 +0300

    Add user routes and controllers

--- backend/src/index.js
+++ backend/src/index.js
@@ -36,17 +36,17 @@ app.use("/api/songs", songRoutes);
 app.use("/api/albums", albumRoutes);
 app.use("/api/stats", statRoutes);
 app.use((err, req, res, next) => {
-  res
-    .status(500)
-    .json({
-      message:
-        process.env.NODE_ENV === "production"
-          ? "Internal server error"
-          : err.message,
-    });
+  res.status(500).json({
+    message:
+      process.env.NODE_ENV === "production"
+        ? "Internal server error"
+        : err.message,
+  });
 });
 
 app.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
   connectDB();
 });
+
+// todo: socket.io real-time communication
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { getAllUsers } from "../controller/user.controller.js";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("User rout with GET method");
-});
+router.get("/", protectRoute, getAllUsers);
+// todo: get messages
 
 export default router;
