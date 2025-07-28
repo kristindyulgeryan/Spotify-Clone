f1cec696abb05eb96aae943b85a0bcd2feeed527
Kristin <kristindyulgeryan@abv.bg>
2025-07-28 14:07:11 +0300

    Add album controller methods to get all albums and get album by ID with populated songs

--- backend/src/controller/admin.controller.js
+++ backend/src/controller/admin.controller.js
@@ -1,4 +1,4 @@
-import { Album } from "../models/album.model .js";
+import { Album } from "../models/album.model.js";
 import { Song } from "../models/song.model.js";
 import cloudinary from "../lib/cloudinary.js";
 
