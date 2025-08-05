e446f53853a09f7b04b47c066696732662c40aad
Kristin <kristindyulgeryan@abv.bg>
2025-08-05 16:33:02 +0300

    Show resizable FriendsActivity sidebar only on non-mobile devices

--- frontend/src/layout/MainLayout.tsx
+++ frontend/src/layout/MainLayout.tsx
@@ -7,9 +7,22 @@ import { Outlet } from "react-router-dom";
 import LeftSidebar from "./components/LeftSidebar.tsx";
 import FriendsActivity from "./components/FriendsActivity.tsx";
 import AudioPlayer from "./components/AudioPlayer.tsx";
+import PlaybackControls from "./components/PlaybackControls.tsx";
+import { useEffect, useState } from "react";
 
 const MainLayout = () => {
-  const isMobile = false;
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkMobile = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    checkMobile();
+    window.addEventListener("resize", checkMobile);
+    return () => window.removeEventListener("resize", checkMobile);
+  }, []);
+
   return (
     <div className="h-screen bg-black text-white flex flex-col">
       <ResizablePanelGroup
@@ -34,18 +47,24 @@ const MainLayout = () => {
           <Outlet />
         </ResizablePanel>
 
-        <ResizableHandle className="w-2 bg-black rounded-lg transition-colors" />
-
-        {/* right sidebar */}
-        <ResizablePanel
-          defaultSize={20}
-          minSize={0}
-          maxSize={25}
-          collapsedSize={0}
-        >
-          <FriendsActivity />
-        </ResizablePanel>
+        {!isMobile && (
+          <>
+            {" "}
+            <ResizableHandle className="w-2 bg-black rounded-lg transition-colors" />
+            {/* right sidebar */}
+            <ResizablePanel
+              defaultSize={20}
+              minSize={0}
+              maxSize={25}
+              collapsedSize={0}
+            >
+              <FriendsActivity />
+            </ResizablePanel>
+          </>
+        )}
       </ResizablePanelGroup>
+
+      <PlaybackControls />
     </div>
   );
 };
