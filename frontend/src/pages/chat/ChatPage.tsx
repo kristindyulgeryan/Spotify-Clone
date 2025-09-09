ee51939762463f26f09e20011d25f094a11f0d66
Kristin <kristindyulgeryan@abv.bg>
2025-09-09 17:19:44 +0300

    Fetches users on mount and messages when a user is selected

--- frontend/src/pages/chat/ChatPage.tsx
+++ frontend/src/pages/chat/ChatPage.tsx
@@ -1,5 +1,29 @@
+import { useChatStore } from "@/stores/useChatStore.ts";
+import { useUser } from "@clerk/clerk-react";
+import { useEffect } from "react";
+import UsersList from "./components/UsersList.tsx";
+import Topbar from "@/components/Topbar";
+
 const ChatPage = () => {
-  return <div>ChatPage</div>;
+  const { user } = useUser();
+  const { messages, selectedUser, fetchUsers, fetchMessages } = useChatStore();
+
+  useEffect(() => {
+    if (user) fetchUsers();
+  }, [fetchUsers, user]);
+
+  useEffect(() => {
+    if (selectedUser) fetchMessages(selectedUser.clerkId);
+  }, [selectedUser, fetchMessages]);
+
+  return (
+    <main className="h-full rounded-lg bg-gradient-to-b from-zinc-800 overflow-hidden">
+      <Topbar />
+      <div className="grid lg:grid-cols-[300px_1fr] grid-cols-[80px_1fr] h-[calc[100vh-180px]]">
+        <UsersList />
+      </div>
+    </main>
+  );
 };
 
 export default ChatPage;
