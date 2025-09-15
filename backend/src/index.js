ad9db2cb09292c1e0bc13b55c3b1decd3672baa9
Kristin <kristindyulgeryan@abv.bg>
2025-09-15 14:09:21 +0300

    Added static file serving for frontend `dist` folder when NODE_ENV is "production"

--- backend/src/index.js
+++ backend/src/index.js
@@ -48,6 +48,15 @@ app.use("/api/auth", authRoutes);
 app.use("/api/songs", songRoutes);
 app.use("/api/albums", albumRoutes);
 app.use("/api/stats", statRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(__dirname, "../frontend/dist/index.html");
+  });
+}
+
+// error handler
 app.use((err, req, res, next) => {
   res.status(500).json({
     message:
